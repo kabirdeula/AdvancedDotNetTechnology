34ebf074105afa1e30f804b91a050ae6329a1147
Kabir Deula <kabirdeula167@gmail.com>
2023-12-20 21:05:24 +0545

    added Lab 02

---
+++ Lab/2023/1220/Lab02/Program.cs
@@ -0,0 +1,20 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        FileStream fileInput = new FileStream("C:\\Users\\kabir\\test.txt", FileMode.OpenOrCreate);
+        fileInput.WriteByte(12);
+        fileInput.WriteByte(6);
+        fileInput.WriteByte(30);
+        fileInput.Close();
+
+        FileStream fileOutput = new FileStream("C:\\Users\\kabir\\test.txt", FileMode.OpenOrCreate);
+        int i = 0;
+        Console.WriteLine("The contents of File are: ");
+        while ((i = fileOutput.ReadByte()) != -1)
+        {
+            Console.WriteLine(i);
+        }
+        fileOutput.Close();
+    }
+}
